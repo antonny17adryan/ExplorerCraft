153320038001d56bc6f9cbf3982ee97c88c32966
antonny17adryan <antonny.adryan@escolar.ifrn.edu.br>
2024-12-13 18:57:20 -0300

    Update desenvolvedor.js

--- desenvolvedor.js
+++ desenvolvedor.js
@@ -1,19 +1,14 @@
 function trocarFoto(id, imagemOriginal, novaImagem) {
     var imagem = document.getElementById(id);
-    
-    // Adiciona a animação de rotação
+    
     imagem.classList.add('rotate');
-    
-    // Verifica o estado atual da imagem
-    if (imagem.src.includes(imagemOriginal)) {
-      // Se estiver na imagem original, troca para a nova imagem
+    
+    if (imagem.src.includes(imagemOriginal)){
       imagem.src = novaImagem;
-    } else {
-      // Se estiver na nova imagem, volta à imagem original
+    } else {
       imagem.src = imagemOriginal;
     }
-  
-    // Remove a animação após o término
+  
     setTimeout(function() {
       imagem.classList.remove('rotate');
     }, 600); // Tempo de animação de rotação (600ms)
